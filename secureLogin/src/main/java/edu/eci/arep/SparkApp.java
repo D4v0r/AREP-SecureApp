486377eda049802f591827e15fbb0b55893145d0
D4v0r <davorcortes.dc@gmail.com>
2020-10-10 12:09:00 -0500

    avance

--- secureLogin/src/main/java/edu/eci/arep/SparkApp.java
+++ secureLogin/src/main/java/edu/eci/arep/SparkApp.java
@@ -10,6 +10,15 @@ public class SparkApp
 {
     public static void main(String[] args) {
         port(getPort());
+        secure("keystores/ecikeystore.p12", "ecistore2", "keystore/trustStore", "ecistore");
+
+        before(((request, response) -> {
+            boolean authenticated = request.session().attribute("Loged");
+
+            if(!authenticated){
+                halt(401, "you not welcome here");
+            }
+        }));
         get("/hello", (req, res) -> "Hello Heroku");
     }
     static int getPort() {
